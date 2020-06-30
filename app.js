40622742f77b8b9a0d63fe84af2b2ac811e6a372
yn8n <ynafuchi@outlook.com>
2020-06-30 23:09:39 +0900

    練習問題

--- app.js
+++ app.js
@@ -56,7 +56,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', routes);
-app.use('/users', users);
+// app.use('/users', users);
+app.use('./users',ensureAuthenticated,usersRouter);
 app.use('/photos', photos);
 
 app.get('/auth/github',
@@ -78,7 +79,10 @@ app.get('/logout', function (req, res) {
   req.logout();
   res.redirect('/');
 });
-
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
